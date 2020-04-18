d0b379cc79d39a68e97a3ea765507e1805ba8f83
ddvk <ddvk>
2020-04-19 00:55:30 +0200

    upload/list somewhat working

--- go/app.go
+++ go/app.go
@@ -1,12 +1,25 @@
 package main
 
-import "io"
-import "golang.org/x/net/websocket"
-import "github.com/gin-gonic/gin"
-import "fmt"
-import "os"
-import "net/http"
-import "io/ioutil"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/net/websocket"
+)
+
+//todo: config
+const (
+	dataDir     = "data"
+	uploadUrl   = "http://localhost:3000"
+	defaultHost = "local.appspot.com"
+)
 
 func EchoServer(ws *websocket.Conn) {
 	b := []byte("ABC")
@@ -14,8 +27,18 @@ func EchoServer(ws *websocket.Conn) {
 	ws.Write(b)
 }
 
-const defaultHost = "local.appspot.com"
-
+type updateStatusRequest struct {
+	ID             string `json:"ID"`
+	Parent         string `json:"Parent"`
+	Version        int    `json:"Version"`
+	Message        string `json:"Message"`
+	Success        bool   `json:"Success"`
+	ModifiedClient string `json:"ModifiedClient"`
+	Type           string `json:"Type"`
+	VissibleName   string `json:"VissibleName"`
+	CurrentPage    int    `json:"CurrentPage"`
+	Bookmarked     bool   `json:"Bookmarked"`
+}
 type rawDocument struct {
 	ID                string `json:"ID"`
 	Version           int    `json:"Version"`
@@ -109,25 +132,40 @@ func main() {
 	})
 
 	r.PUT("/document-storage/json/2/upload/request", func(c *gin.Context) {
-		var json []documentRequest
-		if err := c.ShouldBindJSON(&json); err != nil {
+		var req []documentRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
-		id := json[0].Id
-		if id == "" {
-			id = "someid"
+		response := []documentRequest{}
+		for _, r := range req {
+			id := r.Id
+			if id == "" {
+				id = "someid"
+			}
+			url := uploadUrl + "/upload?id=" + id
+			fmt.Println(url)
+			dr := documentRequest{BlobUrlPut: url, Id: id, Success: true, Version: 1}
+			response = append(response, dr)
 		}
-
-		response := documentRequest{BlobUrlPut: fmt.Sprintf("http://localhost:3000/upload?id=%v", id), Id: id, Success: true, Version: 1}
 
 		c.JSON(200, response)
 	})
 
-	r.GET("/upload", func(c *gin.Context) {
+	r.PUT("/upload", func(c *gin.Context) {
 
 		id := c.Query("id")
+		body := c.Request.Body
+		defer body.Close()
+		fullPath := path.Join(dataDir, fmt.Sprintf("%s.zip", id))
+		file, err := os.Create(fullPath)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		defer file.Close()
+		io.Copy(file, body)
+
 		fmt.Printf("Request: %s\n", id)
 		c.JSON(200, gin.H{})
 	})
@@ -136,15 +174,39 @@ func main() {
 
 		id := c.Query("id")
 		fmt.Printf("Request: %s\n", id)
-		content, err := ioutil.ReadFile("test.zip")
-		if err != nil {
+
+		fullPath := path.Join(dataDir, fmt.Sprintf("%s.zip", id))
+
+		//content, err := ioutil.ReadFile("test.zip")
+		// if err != nil {
+		// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		// 	return
+		// }
+		c.File(fullPath)
+		//c.Data(200, "octetstream/binary", content)
+	})
+
+	r.PUT("/document-storage/json/2/upload/update-status", func(c *gin.Context) {
+		var req []updateStatusRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		c.Data(200, "octetstream/binary", content)
-	})
+		for _, r := range req {
+			path := path.Join(dataDir, fmt.Sprintf("%s.metadata", r.ID))
+			file, err := os.Create(path)
+			if err != nil {
+				return
+				// log
+			}
+			defer file.Close()
 
-	r.GET("/document-storage/json/2/upload/update-status", func(c *gin.Context) {
+			js, err := json.Marshal(r)
+			file.Write(js)
+			if err != nil {
+				// log
+			}
+		}
 
 		c.String(200, "")
 	})
@@ -156,7 +218,10 @@ func main() {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		fmt.Println(json)
+
+		for _, j := range json {
+			fmt.Println(j)
+		}
 		c.JSON(200, gin.H{})
 	})
 
@@ -164,10 +229,49 @@ func main() {
 		withBlob := c.Query("withBlob")
 		docId := c.Query("docId")
 		fmt.Println(withBlob, docId)
-		response := rawDocument{VissibleName: "stuff"}
+		result := []rawDocument{}
 
-		c.JSON(200, response)
+		files, err := ioutil.ReadDir(dataDir)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, f := range files {
+			ext := filepath.Ext(f.Name())
+			fmt.Println(ext)
+			if ext != ".metadata" {
+				continue
+			}
+			fullPath := path.Join(dataDir, f.Name())
+			f, err := os.Open(fullPath)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer f.Close()
+			content, err := ioutil.ReadAll(f)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			response := rawDocument{}
+			response.BlobURLGet = uploadUrl + "/download?id=" + response.ID
+			err = json.Unmarshal(content, &response)
+			if err != nil {
+			}
+			result = append(result, response)
+		}
+
+		c.JSON(200, result)
 	})
+
+	err := os.MkdirAll(dataDir, 0600)
+
+	if err != nil {
+		panic(err)
+	}
 
 	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
