ff3db1f0fa4f6d87373c0c68dbce6edf5d1d9462
ddvk <ddvk>
2020-04-21 00:15:42 +0200

    fix hosts script

--- device/patchhosts.sh
+++ device/patchhosts.sh
@@ -1,4 +1,5 @@
-if [ ! grep patched /etc/hosts ]; then
+#!/bin/sh
+if  ! grep patched /etc/hosts ; then
 cat <<EOF >> /etc/hosts
 # patched
 127.0.0.1 service-manager-production-dot-remarkable-production.appspot.com
