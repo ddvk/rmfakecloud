997dc56990daeade56d82334203142200aec2b47
ddvk <ddvk>
2020-04-24 00:11:50 +0200

    fix script

--- device/fixsync.sh
+++ device/fixsync.sh
@@ -1,2 +1,2 @@
 # mark files as not synced
-grep sync .local/share/remarkable/xochitl/*.metadata -l | xargs sed -i 's/synced\": true/synced\": false/'
+grep sync ~/.local/share/remarkable/xochitl/*.metadata -l | xargs sed -i 's/synced\": true/synced\": false/'
