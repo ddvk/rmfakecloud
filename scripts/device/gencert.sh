d7531d9c7a6f18017fd39c4fb854bd5281ca6362
ddvk <ddvk>
2020-09-18 17:50:53 +0200

    cert gen skip existing

--- scripts/device/gencert.sh
+++ scripts/device/gencert.sh
@@ -36,24 +36,46 @@ DNS.2 = my.remarkable.com
 # DNS.3 = any additional hosts
 EOF
 
-# host
-echo "Generating proxy keys..."
-openssl genrsa -out proxy.key 2048
-openssl rsa -in proxy.key -pubout -out proxy.pubkey
-openssl req -new -config ./csr.conf -key proxy.key -out proxy.csr 
-
-
 # ca
-echo "Generating ca..."
-openssl genrsa -out ca.key 2048
-openssl req -new -sha256 -x509 -key ca.key -out ca.crt -days 3650 -subj /CN=rmfakecloud
+if [ ! -f ca.crt ]; then 
+    echo "Generating ca..."
+    openssl genrsa -out ca.key 2048
+    openssl req -new -sha256 -x509 -key ca.key -out ca.crt -days 3650 -subj /CN=rmfakecloud
+    rm proxy.key || true
+    rm proxy.pubkey || true
+else
+    echo "CA exists"
+fi
 
-# Signing
-openssl x509 -req  -in proxy.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out proxy.crt -days 3650 -extfile csr.conf -extensions caext
-cat proxy.crt ca.crt > proxy.bundle.crt
+if [ ! -f proxy.key ]; then 
+    echo "Generating proxy keys..."
+    openssl genrsa -out proxy.key 2048
+    rm proxy.pubkey || true
+else
+    echo "Private key exists"
+fi
 
-echo "showing result"
-openssl x509 -in proxy.bundle.crt -text -noout 
+if [ ! -f proxy.pubkey ]; then 
+    openssl rsa -in proxy.key -pubout -out proxy.pubkey
+    rm proxy.crt || true
+else
+    echo "Pub key exists"
+fi
 
-echo "Generation complete"
+if [ ! -f proxy.crt ]; then 
+    openssl req -new -config ./csr.conf -key proxy.key -out proxy.csr 
+
+    # Signing
+    openssl x509 -req  -in proxy.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out proxy.crt -days 3650 -extfile csr.conf -extensions caext
+    #cat proxy.crt ca.crt > proxy.bundle.crt
+
+    echo "showing result"
+    openssl x509 -in proxy.bundle.crt -text -noout 
+
+    echo "Generation complete"
+else
+    echo "crt exists"
+fi
+
+
 
