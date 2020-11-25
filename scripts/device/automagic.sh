9c91d7889a3d558ad57b0a95964afeb3b1080d10
ddvk <ddvk>
2020-11-25 09:43:19 +0100

    add cleanup script

--- scripts/device/automagic.sh
+++ scripts/device/automagic.sh
@@ -6,7 +6,7 @@ REPOURL="https://github.com/ddvk/rmfakecloud/raw/master/scripts/device/"
 if [ -z "$SKIP_DOWNLOAD" ]; then
     systemctl stop proxy || true
     echo "Getting assets..."
-    assets=(secure install.sh fixsync.sh gencert.sh patchhosts.sh installcert.sh installproxy.sh )
+    assets=(secure install.sh fixsync.sh gencert.sh patchhosts.sh installcert.sh installproxy.sh cleanup.sh)
     for app in "${assets[@]}"
     do
         wget "$REPOURL/$app" -O $app
