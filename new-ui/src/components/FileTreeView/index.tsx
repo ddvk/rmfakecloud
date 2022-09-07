0b529e53620a25798bce244d069e7edf2551a8b5
zgs225 <zgs225@gmail.com>
2022-09-07 22:31:51 +0800

    chore(ui): replace some icons

--- new-ui/src/components/FileTreeView/index.tsx
+++ new-ui/src/components/FileTreeView/index.tsx
@@ -6,7 +6,7 @@ import {
   PencilAltIcon,
   TrashIcon,
   FolderIcon
-} from '@heroicons/react/solid'
+} from '@heroicons/react/outline'
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Transition } from '@headlessui/react'
