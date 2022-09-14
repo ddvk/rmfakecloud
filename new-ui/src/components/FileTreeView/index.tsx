8837473f1600bd5d6a10d45866476199544df7e0
zgs225 <zgs225@gmail.com>
2022-09-14 16:08:14 +0800

    chore(ui): improve FileMenu style

--- new-ui/src/components/FileTreeView/index.tsx
+++ new-ui/src/components/FileTreeView/index.tsx
@@ -151,20 +151,20 @@ function FileMenu(params: FileMenuProps) {
         <div className="mx-auto max-w-4xl border-t border-slate-100/20 md:border-none">
           <div className="rounded bg-slate-900 md:mx-4 md:border-x md:border-t md:border-slate-100/20">
             <div className="flex justify-between md:justify-around">
-              <div className="cursor-pointer p-4 hover:text-neutral-200">
+              <div className="basis-1/4 cursor-pointer p-4 hover:text-neutral-200">
                 <SaveIcon className="mx-auto mb-1 h-6 w-6" />
-                <p className="text-xs">{t('documents.file_tree_view.menu.download')}</p>
+                <p className="text-center text-xs">{t('documents.file_tree_view.menu.download')}</p>
               </div>
-              <div className="cursor-pointer p-4 hover:text-neutral-200">
+              <div className="basis-1/4 cursor-pointer p-4 hover:text-neutral-200">
                 <EyeIcon className="mx-auto mb-1 h-6 w-6" />
-                <p className="text-xs">{t('documents.file_tree_view.menu.view')}</p>
+                <p className="text-center text-xs">{t('documents.file_tree_view.menu.view')}</p>
               </div>
-              <div className="cursor-pointer p-4 hover:text-neutral-200">
+              <div className="basis-1/4 cursor-pointer p-4 hover:text-neutral-200">
                 <PencilAltIcon className="mx-auto mb-1 h-6 w-6" />
-                <p className="text-xs">{t('documents.file_tree_view.menu.rename')}</p>
+                <p className="text-center text-xs">{t('documents.file_tree_view.menu.rename')}</p>
               </div>
               <div
-                className="cursor-pointer p-4 hover:text-neutral-200"
+                className="basis-1/4 cursor-pointer p-4 hover:text-neutral-200"
                 onClick={() => {
                   if (!doc) {
                     return
@@ -174,7 +174,7 @@ function FileMenu(params: FileMenuProps) {
                 }}
               >
                 <TrashIcon className="mx-auto mb-1 h-6 w-6" />
-                <p className="text-xs">{t('documents.file_tree_view.menu.remove')}</p>
+                <p className="text-center text-xs">{t('documents.file_tree_view.menu.remove')}</p>
               </div>
             </div>
           </div>
