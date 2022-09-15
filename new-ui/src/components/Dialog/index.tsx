1b5f9f5606a30275be26a7e97092cfb52e54c8d4
zgs225 <zgs225@gmail.com>
2022-09-15 16:29:31 +0800

    chore(ui): adjust dialog

--- new-ui/src/components/Dialog/index.tsx
+++ new-ui/src/components/Dialog/index.tsx
@@ -36,7 +36,7 @@ export function ConfirmationDialog({ isOpen, onClose, ...params }: ConfirmationD
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black/50" />
+          <div className="fixed inset-0 bg-black/50 backdrop-blur" />
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-y-auto">
