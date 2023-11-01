cd681805759d2222c2c39949c81ce31125c8a0b3
doebi <christoph@doeberl.at>
2023-11-01 11:22:09 +0100

    added metadata to file list/grid

--- ui/src/pages/Documents/FileList.js
+++ ui/src/pages/Documents/FileList.js
@@ -1,5 +1,18 @@
 import { Stack } from "react-bootstrap";
 import FileIcon from "./FileIcon";
+import moment from "moment";
+
+// optional: load user-specific locale from config
+// moment.locale(locale); // eg. 'de' or 'fr'
+
+
+function formatBytes(bytes) {
+  if(bytes === 0) return '0 Bytes';
+  let k = 1024,
+    sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+    i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
 
 export default function FileListViewer({ listStyle, files, onSelect }) {
 
@@ -7,17 +20,31 @@ export default function FileListViewer({ listStyle, files, onSelect }) {
     onSelect(file.id);
   }
 
-  const itemClassName = (item) => {
+  const isFolderClassName = (item) => {
     if (item.isFolder) return "is-folder";
     return "";
   }
 
   const listItems = files.map(file =>
-    <div className="filelist-item p-2" key={file.id} onClick={() => onClickItem(file)}>
-      <div className={itemClassName(file)}>
-        <FileIcon file={file} />
-        {file.name}
-      </div>
+    <div className="filelist-item p-2" key={file.id} onClick={() => onClickItem(file)} style={{ borderBottom: '1px solid #333' }}>
+      <Stack direction="horizontal">
+        <div>
+          <FileIcon file={file} />
+        </div>
+        <div className={isFolderClassName(file)}>
+          {file.name}
+        </div>
+        <div style={{ flex: 1 }}>
+        </div>
+        <div className="filelist-metadata">
+          {file.isFolder && <span>{file.children.length || "empty"}</span> }
+          {!file.isFolder && <span>{formatBytes(file.size)}</span> }
+        </div>
+        <div className="filelist-metadata">
+          {moment(file.lastModified).format('L')}{" "}
+          {moment(file.lastModified).format('LT')}
+        </div>
+      </Stack>
     </div>
   );
 
@@ -27,6 +54,11 @@ export default function FileListViewer({ listStyle, files, onSelect }) {
         <FileIcon file={file} />
         {file.name}
       </div>
+      {/*
+      <div className="filegrid-metadata">
+        {file.children.length || "empty"}
+      </div>
+      */}
     </div>
   );
 
@@ -38,13 +70,19 @@ export default function FileListViewer({ listStyle, files, onSelect }) {
       <div className="filename">
         {file.name}
       </div>
+      <div className="filegrid-metadata">
+        <span>{formatBytes(file.size)}</span>
+      </div>
     </div>
   );
 
   return (
     <>
       {files && (listStyle === "list") && (
-        <Stack className="filelist">{listItems}</Stack>
+        <div>
+          <div style={{ height: '1em', width: '100%', borderBottom: '1px solid #333' }}></div>
+          <Stack className="filelist">{listItems}</Stack>
+        </div>
       )}
 
       {files && (listStyle === "grid") && (
