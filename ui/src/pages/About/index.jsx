66ea008904e58f8c07dd3865b8ba638f1135dd8d
davidpkj <davidpenkow1@gmail.com>
2024-09-05 16:01:24 +0200

    Update UI About content

--- ui/src/pages/About/index.jsx
+++ ui/src/pages/About/index.jsx
@@ -4,19 +4,44 @@ import Container from "react-bootstrap/Container";
 const Home = () => {
   return (
     <Container>
-      <div>
-        <h3>Welcome to your own rm cloud</h3>
-        <h3>About</h3>
-        <p>This is still a work in progress</p>
-        <h3>TIPS</h3>
+      <main>
+        <h1>Welcome to your own reMarkable Cloud!</h1>
+        <h2>About</h2>
+        <p>
+          This software is an unofficial replacement for the proprietary
+          reMarkable Cloud.  In case you want to sync/backup your files
+          and have full control of the hosting environment, this is the
+          software for you.
+        </p>
+        <p>
+          It's is still a work in progress being actively maintained over
+          on <a href="https://github.com/ddvk/rmfakecloud">GitHub</a>.
+        </p>
+        <h2>Tips</h2>
         <ul>
           <li>
-            you can use rmapi (https://github.com/juruen/rmapi) for managing
-            files, just specify the URL of your instance with the RMAPI_HOST
-            variable (RMAPI_HOST=https//rmfakeclud.example.com rmapi)
+            <p>
+              You can use <a href="https://github.com/juruen/rmapi">rmapi</a>
+              for managing files, just specify the URL of your instance with
+              the RMAPI_HOST variable like
+              so: <code>RMAPI_HOST=https//rmfakeclud.example.com rmapi</code>
+            </p>
+          </li>
+          <li>
+            <p>
+              Check out
+              the <a href="https://ddvk.github.io/rmfakecloud/">Online Docs</a>
+              if you have Questions on the configuration options.
+            </p>
+          </li>
+          <li>
+            <p>
+              We support the Read on reMarkable Extension. Read more about it
+              in the online Documentation.
+            </p>
           </li>
         </ul>
-      </div>
+      </main>
     </Container>
   );
 };
