09046d2154ab6771043c422fa9324620f10c97a3
davidpkj <davidpenkow1@gmail.com>
2024-09-05 21:07:42 +0200

    Update Home page tips

--- ui/src/pages/Home/index.jsx
+++ ui/src/pages/Home/index.jsx
@@ -13,30 +13,33 @@ const Home = () => {
           software for you.
         </p>
         <p>
-          It's is still a work in progress being actively maintained over
+          It's is still a work in progress being, actively maintained over
           on <a href="https://github.com/ddvk/rmfakecloud">GitHub</a>.
         </p>
         <h2>Tips</h2>
         <ul>
           <li>
             <p>
-              You can use <a href="https://github.com/juruen/rmapi">rmapi</a>
-              for managing files, just specify the URL of your instance with
-              the RMAPI_HOST variable like
+              You can use <a href="https://github.com/juruen/rmapi">rmapi</a> for managing files,
+              just specify the URL of your instance with the RMAPI_HOST variable like
               so: <code>RMAPI_HOST=https//rmfakeclud.example.com rmapi</code>
             </p>
           </li>
           <li>
             <p>
-              Check out
-              the <a href="https://ddvk.github.io/rmfakecloud/">Online Docs</a>
-              if you have Questions on the configuration options.
+              Check out the online <a href="https://ddvk.github.io/rmfakecloud/">documentation</a> to
+              learn more about the configuration options. Also read the README
             </p>
           </li>
           <li>
             <p>
-              We support the Read on reMarkable Extension. Read more about it
-              in the online Documentation.
+              You should also read the <a href="https://github.com/ddvk/rmfakecloud/blob/master/README.md">README</a>,
+              to see the current status of the project and notes from the developers.
+            </p>
+          </li>
+          <li>
+            <p>
+              We support the Read on reMarkable Extension. Read more about it in the online documentation.
             </p>
           </li>
           <li>
