c2bfcf760c9d798d5c400c61fd75125289c45ef5
ddvk <36803246+ddvk@users.noreply.github.com>
2024-11-17 12:48:30 +0100

    fix js warnings

--- ui/src/pages/Admin/index.jsx
+++ ui/src/pages/Admin/index.jsx
@@ -1,16 +1,8 @@
 import Container from "react-bootstrap/Container";
 import Stack from "react-bootstrap/Stack";
-import { useAuthState } from "../../common/useAuthContext";
-
 import UserList from "./UserList";
 
 const Home = () => {
-  const { state: { user } } = useAuthState();
-
-  function isAdmin(user) {
-    return user && user.Roles && user.Roles[0] === "Admin";
-  }
-
   return (
     <Container fluid>
       <Stack>
