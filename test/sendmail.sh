7b67df1d5deaf0b769a0aee2cb3f25b4bd4b890c
ddvk <36803246+ddvk@users.noreply.github.com>
2024-11-23 13:19:10 +0100

    codeql action v3

--- test/sendmail.sh
+++ test/sendmail.sh
@@ -7,11 +7,12 @@ cd `dirname $0`
 RECIPIENT=${1:-test@blah}
 
 TOKEN=$(./getusertoken.sh)
+BODY="blah<script>alert(1)</script>"
 
 curl -F attachment=@test.pdf \
     -F 'from=test@test.com' \
     -F 'to='$RECIPIENT'' \
-    -F 'html=blahblah' \
+	-F 'html='$BODY'' \
     -H "Authorization: Bearer $TOKEN" \
     -sS \
     -X POST $URL/api/v2/document 
