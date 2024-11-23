b01e1578a7df61379bf18ec6c78690990de7561c
ddvk <36803246+ddvk@users.noreply.github.com>
2024-11-23 15:08:39 +0100

    docker workflow

--- scripts/rundocker.sh
+++ scripts/rundocker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 HOSTPORT=3000
 DATA=$(realpath data)
-id=$(docker run -d -p $HOSTPORT:3000 -v $DATA:/data -e STORAGE_URL=http://$(hostname):$HOSTPORT --rm rmfakecloud)
+id=$(docker run -d -p $HOSTPORT:3000 -v $DATA:/data --rm rmfakecloud)
 echo $id
 docker logs $id
